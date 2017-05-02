3792f166147f0e24e42181f016abb56c07b30ea7
FloydAsa <floydmoa@rocketmail.com>
2017-05-02 12:18:04 +0200

    Add files via upload

---
+++ text file.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 02 12:13:59 2017
+
+@author: 214576460
+"""
+
+print 'Floyd Asa'
+print 'floydmoa@rocketmail.com'
+print 'Probing Structure formation through Baryon Acoustic Oscillations from SKA'
+print 'Learn how to code on python'
